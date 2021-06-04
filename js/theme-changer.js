405a258265e97d9ac62c925f2a9ddbe16e1df949
Vatsal Singh <vatsalsinghkv@gmail.com>
2021-06-04 21:23:01 +0530

    v1.0

--- js/theme-changer.js
+++ js/theme-changer.js
@@ -1,15 +1,30 @@
-$('.theme-btns').click(function() {
-    let oldBodyClass = $('body').attr('class');
-    let newBodyClass = $(this).attr('name');
+let themes = {
+    '!': 'theme-1',
+    '@': 'theme-2',
+    '#': 'theme-3'
+};
+
+$(document).keypress((e) => {
+    if(e.key in themes) {
+        changeBodyClass(themes[e.key]);
+    }
+});
+
+$('.theme-btns').click(function(){
+    changeBodyClass($(this).attr('name'));
+});
+
+function changeBodyClass(newClass) {
+    let oldClass = $('body').attr('class');
 
     // hide theme btns
-    $(`.theme-btn-${getBtnNum(oldBodyClass)}`).removeClass('show');
-    $(`.theme-btn-${getBtnNum(newBodyClass)}`).addClass('show');
+    $(`.theme-btn-${getBtnNum(oldClass)}`).removeClass('show');
+    $(`.theme-btn-${getBtnNum(newClass)}`).addClass('show');
 
     // change theme of the page
-    $('body').removeClass(oldBodyClass);
-    $('body').addClass(newBodyClass);
-});
+    $('body').removeClass(oldClass);
+    $('body').addClass(newClass);
+}
 
 function getBtnNum(str) {
     return str.slice(str.length-1);
